3b69e1f854c5787dfadec634c5f90ffd57b7ed5d
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-03-21 21:36:34 -0500

    Creación del footer

--- project/src/components/Logo/Logo.jsx
+++ project/src/components/Logo/Logo.jsx
@@ -1,7 +1,9 @@
+import { logo } from "./Logo.module.css"
+
 export const Logo = () => {
   return (
     <div>
-      <h1>SIAT</h1>
+      <img src="https://cdn.pixabay.com/photo/2014/06/03/19/38/board-361516_1280.jpg" alt="Logo" className={logo} />
     </div>
   )
 }
