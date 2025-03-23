5b5f753e11178e671ff3d5725c7e91fc0012b559
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-03-23 17:48:41 -0500

    Validación del formulario de accidentes

--- project/src/AppRouter.jsx
+++ project/src/AppRouter.jsx
@@ -1,10 +1,12 @@
-import { Routes, Route, Navigate } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
+import { AccidentReport } from "./pages/AccidentReport"
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/mapa" />} />
+      <Route path="/" element={<h2>Inicio</h2>} />
       <Route path="/mapa" element={<h2>Mapa</h2>} />
+      <Route path="/registrar-accidente" element={<AccidentReport />} />
     </Routes>
   )
 }
