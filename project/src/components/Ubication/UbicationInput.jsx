cdf7d695da50e6484c98a1460df5b1c3254ce10a
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-03-31 10:51:56 -0500

    Cambios en la ubicación en el mapa

--- project/src/components/Ubication/UbicationInput.jsx
+++ project/src/components/Ubication/UbicationInput.jsx
@@ -1,14 +1,9 @@
-import { cloneElement, useState } from "react"
-import { useApiIsLoaded, useMap, useMapsLibrary } from "@vis.gl/react-google-maps"
+import { cloneElement } from "react"
+import { useAddressMarker } from "../../hooks"
 import { useEffect } from "react"
-import { useRef } from "react"
 
 export const UbicationInput = ({ onPlaceSelect, markerPosition, children }) => {
-  const [address, setAddress] = useState("")
-  const inputRef = useRef(null)
-  const map = useMap()
-  const apiIsLoaded = useApiIsLoaded()
-  const places = useMapsLibrary("places")
+  const { address, setAddress, inputRef, apiIsLoaded } = useAddressMarker(onPlaceSelect)
 
   useEffect(() => {
     if (!markerPosition || !apiIsLoaded || !inputRef.current) return
@@ -18,24 +13,7 @@ export const UbicationInput = ({ onPlaceSelect, markerPosition, children }) => {
       if (status !== "OK" || !results[0]) return
       setAddress(results[0].formatted_address)
     })
-  }, [markerPosition, apiIsLoaded])
-
-  useEffect(() => {
-    if (!apiIsLoaded || !inputRef.current) return
-
-    const autocomplete = new places.Autocomplete(inputRef.current)
-
-    autocomplete.addListener("place_changed", () => {
-      const place = autocomplete.getPlace()
-      if (place.geometry?.location) {
-        const selectedLocation = { lat: place.geometry.location.lat(), lng: place.geometry.location.lng() }
-        onPlaceSelect(selectedLocation)
-        map.panTo(selectedLocation)
-      }
-    })
-
-    return () => autocomplete.unbindAll()
-  }, [apiIsLoaded, onPlaceSelect, places, map])
+  }, [markerPosition, apiIsLoaded, inputRef, setAddress])
 
   return cloneElement(children, { ref: inputRef, value: address, onChange: (e) => setAddress(e.target.value) })
 }
--- project/src/hooks/index.js
+++ project/src/hooks/index.js
@@ -1,3 +1,4 @@
 export * from "./useFetchData"
 export * from "./useModalContext"
 export * from "./useAuthContext"
+export * from "./useAddressMarker"
