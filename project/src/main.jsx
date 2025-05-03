884b9be3305fb264e7e5d07fa7fa4e7699f89934
MaicolRodriguez12 <micheal.arana@correounivalle.edu.co>
2025-05-03 01:22:14 -0500

    perfil de usuario registrado

--- project/src/main.jsx
+++ project/src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from "react-dom/client"
 import { App } from "./App"
 import "./index.css"
 import { axiosService } from "./services"
+import { UserProfile } from "./components/UserProfile/UserProfile"
 
 axiosService.initAxios()
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <UserProfile />
   </StrictMode>,
 )
