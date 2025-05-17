70234525a5d4dd8ade8126b23037921ab1dd3f0b
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-05-17 15:56:16 -0500

    refactor: cambios en los estilos de los inputs en registrar accidente

--- project/src/pages/AccidentReport/components/SelectForm/SelectForm.jsx
+++ project/src/pages/AccidentReport/components/SelectForm/SelectForm.jsx
@@ -1,7 +1,15 @@
 import { Controller } from "react-hook-form"
 import { ErrorForm } from "../ErrorForm"
+import { selectInput } from "./SelectForm.module.css"
 
-export const SelectForm = ({ name, control, label, className, options, error }) => {
+export const SelectForm = ({
+  name,
+  control,
+  label,
+  className,
+  options,
+  error,
+}) => {
   return (
     <div>
       <label htmlFor={name}>{label}</label>
@@ -9,7 +17,11 @@ export const SelectForm = ({ name, control, label, className, options, error }) 
         name={name}
         control={control}
         render={({ field }) => (
-          <select id={name} className={className} {...field}>
+          <select
+            id={name}
+            className={className ? `${selectInput} ${className}` : selectInput}
+            {...field}
+          >
             <option value="">Seleccione una opción</option>
             {options.map((option, index) => (
               <option key={index} value={option.value}>
