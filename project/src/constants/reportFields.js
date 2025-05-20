805d33a451509c647657a6466c72fcb29466dd88
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-05-19 22:11:09 -0500

    feature: conexión del filtro por fecha de accidente

--- project/src/constants/reportFields.js
+++ project/src/constants/reportFields.js
@@ -1,5 +1,6 @@
 export const REPORT_FIELDS = {
   ACCIDENT_TOTAL: "total de accidentes",
+  COUNT: "count",
   REPORT_ID: "ID de reporte",
   CONFIRMED: "Confirmado",
 }
--- project/src/pages/FilterAccidents/components/FilterForm/FilterAccidentsForm.jsx
+++ project/src/pages/FilterAccidents/components/FilterForm/FilterAccidentsForm.jsx
@@ -11,13 +11,13 @@ import {
   formContainer,
   noFilterSelected,
 } from "./FilterAccidentsForm.module.css"
+import { useState } from "react"
 
-export const FilterAccidentsForm = ({ submitData }) => {
+export const FilterAccidentsForm = ({ submitData, filterDate }) => {
   const {
     control,
     handleSubmit,
     watch,
-    reset,
     formState: { errors },
   } = useForm({
     resolver: zodResolver(filterAccidentSchema),
@@ -34,55 +34,61 @@ export const FilterAccidentsForm = ({ submitData }) => {
   })
 
   const onSubmit = handleSubmit((data) => {
-    console.log(data)
-    // submitData(data)
-    reset()
+    submitData(data)
   })
 
   const selectedFilter = watch("filter.type")
 
   return (
-    <FilterForm handleSubmit={onSubmit} className={form}>
-      <div className={formContainer}>
-        <InputForm
-          name="filterDate.startDate"
-          control={control}
-          type="date"
-          label="Fecha Inicial del Accidente"
-          error={errors.filterDate?.startDate}
-        />
-        <InputForm
-          name="filterDate.endDate"
-          control={control}
-          type="date"
-          label="Fecha Final del Accidente"
-          error={errors.filterDate?.endDate}
-        />
-        <SelectForm
-          name="filter.type"
-          control={control}
-          label="Filtro"
-          options={ACCIDENTS_FILTERS.filterAccident}
-          error={errors.filter?.type}
-        />
-        {selectedFilter ? (
-          <FilterOption selectedFilter={selectedFilter}>
-            <InputForm
-              name="filter.value"
-              control={control}
-              type="text"
-              label="Dirección del Accidente"
-              placeholder="Ingrese la dirección del accidente"
-              error={errors.filter?.value}
-              filter="Dirección del Accidente"
-            />
-          </FilterOption>
-        ) : (
-          <div className={noFilterSelected}>
-            <span>No hay filtros seleccionados</span>
-          </div>
-        )}
-      </div>
-    </FilterForm>
+    <>
+      <FilterForm handleSubmit={onSubmit} className={form}>
+        <div className={formContainer}>
+          <InputForm
+            name="filterDate.startDate"
+            control={control}
+            type="date"
+            label="Fecha Inicial del Accidente"
+            error={errors.filterDate?.startDate}
+          />
+          <InputForm
+            name="filterDate.endDate"
+            control={control}
+            type="date"
+            label="Fecha Final del Accidente"
+            error={errors.filterDate?.endDate}
+          />
+          <SelectForm
+            name="filter.type"
+            control={control}
+            label="Filtro"
+            options={ACCIDENTS_FILTERS.filterAccident}
+            error={errors.filter?.type}
+          />
+          {selectedFilter ? (
+            <FilterOption selectedFilter={selectedFilter}>
+              <InputForm
+                name="filter.value"
+                control={control}
+                type="text"
+                label="Dirección del Accidente"
+                placeholder="Ingrese la dirección del accidente"
+                error={errors.filter?.value}
+                filter="Dirección del Accidente"
+              />
+            </FilterOption>
+          ) : (
+            <div className={noFilterSelected}>
+              <span>No hay filtros seleccionados</span>
+            </div>
+          )}
+        </div>
+      </FilterForm>
+      {filterDate && (
+        <p>
+          Filtrando accidentes entre el {filterDate.startDate} y el{" "}
+          {filterDate.endDate}
+        </p>
+      )}
+    </>
   )
 }
